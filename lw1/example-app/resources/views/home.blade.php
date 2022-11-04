ad92bdd019d7834c6ec2d43daafbf3dbc7d6c21f
Iluza11 <92926531+Iluza11@users.noreply.github.com>
2022-11-04 09:12:49 +0300

    initial commit

---
+++ lw1/example-app/resources/views/home.blade.php
@@ -0,0 +1,141 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="{{ asset('resources/css/app.css') }}" rel="stylesheet">
+
+        <title>Laravel</title>
+    </head>
+    <body>
+    <svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+<svg width="140" height="130">
+  <polygon
+    points="70,5 90,41 136,48 103,80 111,126 70,105 29,126 36,80 5,48 48,41"
+    fill="#FFA500" stroke="#FF8C00" stroke-width="5"
+  />
+</svg>
+
+        <div class="text1" style="
+        background-color: #FFE4C4;
+        margin-top: 250px;
+        margin-left: 400px;
+        margin-right: 1200px; 
+        border: 8px double #FFA500; 
+        padding: 5px; 
+        border-radius: 10px;
+        text-decoration: underline;">{{ $dorama1 }}<br> 
+        {{ $description1 }} 
+        </div>
+        <div class="text2" style="
+        background-color: #FFE4C4;
+        margin-top: -160px;
+        margin-left: 1100px;
+        margin-right: 500px; 
+        border: 8px double #FFA500; 
+        padding: 5px; 
+        border-radius: 10px;
+        text-decoration: underline;">{{ $dorama2 }}<br> 
+        {{ $description2 }} 
+        </div>
+        <div class="text3" style="
+        background-color: #FFE4C4;
+        margin-top: 130px;
+        margin-left: 400px;
+        margin-right: 1200px; 
+        border: 8px double #FFA500; 
+        padding: 5px; 
+        border-radius: 10px;
+        text-decoration: underline;">{{ $dorama3 }}<br> 
+        {{ $description3 }} 
+        </div>
+        <div class="text4" style="
+        background-color: #FFE4C4;
+        margin-top: -200px;
+        margin-left: 1100px;
+        margin-right: 500px;
+        border: 8px double #FFA500;
+        padding: 5px;
+        border-radius: 10px;
+        text-decoration: underline;">{{ $dorama4 }}<br> 
+        {{ $description4 }} 
+    </div>
+    </body>
+</html>
