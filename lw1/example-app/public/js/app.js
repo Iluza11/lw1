e60374d005fe3e804b3fd3b030af081de41454b2
Iluza11 <92926531+Iluza11@users.noreply.github.com>
2022-11-10 19:55:25 +0300

    initial commit

--- lw1/example-app/public/js/app.js
+++ lw1/example-app/public/js/app.js
@@ -19437,8 +19437,8 @@ window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 /*! no static exports found */
 /***/ (function(module, exports, __webpack_require__) {
 
-__webpack_require__(/*! C:\Users\user\example-app\resources\js\app.js */"./resources/js/app.js");
-module.exports = __webpack_require__(/*! C:\Users\user\example-app\resources\css\app.css */"./resources/css/app.css");
+__webpack_require__(/*! C:\Users\user\Desktop\lw1\lw1\example-app\resources\js\app.js */"./resources/js/app.js");
+module.exports = __webpack_require__(/*! C:\Users\user\Desktop\lw1\lw1\example-app\resources\css\app.css */"./resources/css/app.css");
 
 
 /***/ })
